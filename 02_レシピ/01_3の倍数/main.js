1ea29d90ba9ef5d2c0b01aae31527d37d26a700c
kekehoho <tkbys.univ01061@outlook.jp>
2022-10-06 15:56:11 +0900

    FizzBuzz

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,11 +1,15 @@
 const genkiFunction = function (number) {
   for (let n = 1; n <= number; n++) {
-    if (n % 3 === 0) {
-      console.log(n + "!!!!!!!")
+    if (n % 15 === 0) {
+      console.log("FizzBuzz")
+    } else if (n % 3 === 0) {
+      console.log("Fizz")
+    } else if (n % 5 === 0) {
+      console.log("Buzz")
     } else {
       console.log(n)
     }
   }
 }
 
-genkiFunction(48)
+genkiFunction(100)
