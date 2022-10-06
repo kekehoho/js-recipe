03aa01f9e661109e1081ead477b8a206d1c5f54e
kekehoho <tkbys.univ01061@outlook.jp>
2022-10-06 16:11:44 +0900

    ３が大好き

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,11 +1,28 @@
+//FizzBuzz//
+//-----------------------------------------------//
+// const genkiFunction = function (number) {
+//   for (let n = 1; n <= number; n++) {
+//     if (n % 15 === 0) {
+//       console.log("FizzBuzz")
+//     } else if (n % 3 === 0) {
+//       console.log("Fizz")
+//     } else if (n % 5 === 0) {
+//       console.log("Buzz")
+//     } else {
+//       console.log(n)
+//     }
+//   }
+// }
+//-----------------------------------------------//
+
+//favorite is 3
+//------------------------------------------------//
 const genkiFunction = function (number) {
   for (let n = 1; n <= number; n++) {
-    if (n % 15 === 0) {
-      console.log("FizzBuzz")
-    } else if (n % 3 === 0) {
-      console.log("Fizz")
-    } else if (n % 5 === 0) {
-      console.log("Buzz")
+    if (n % 3 === 0) {
+      console.log(n + "!!!!!!!!")
+    } else if (/3/.test(n)) {
+      console.log(n + "!!!!!!!!")
     } else {
       console.log(n)
     }
