7887d1911cfd996a56691a9eaa76537a0dd5df2a
kekehoho <tkbys.univ01061@outlook.jp>
2022-10-07 11:57:34 +0900

    便利なボタン

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,10 +1,24 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+const twiceButton = document.getElementById("twice-button")
 
 let count = 0
 
 plusButton.onclick = function () {
   count += 1
+
+  display.textContent = count
+}
+
+minusButton.onclick = function () {
+  count -= 1
+
+  display.textContent = count
+}
+
+twiceButton.onclick = function () {
+  count *= 2
 
   display.textContent = count
 }
