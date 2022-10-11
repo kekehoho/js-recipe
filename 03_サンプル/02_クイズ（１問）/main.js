9d4c4d90575c6cd8712ccf77c7bea6c59ee09f07
kekehoho <tkbys.univ01061@outlook.jp>
2022-10-12 02:04:38 +0900

    マイクイズ

--- 03_サンプル/02_クイズ（１問）/main.js
+++ 03_サンプル/02_クイズ（１問）/main.js
@@ -6,22 +6,79 @@ const choice3 = document.getElementById("choice-3")
 const feedback = document.getElementById("feedback")
 
 // クイズの内容
+// const quiz = {
+//   text: "この星の名前は何でしょう？",
+//   image: "Ganymede.jpg",
+//   choices: [
+//     {
+//       text: "ゴリアテ",
+//       feedback:
+//         "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。",
+//     },
+//     {
+//       text: "ゼニガメ",
+//       feedback: "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。",
+//     },
+//     {
+//       text: "ガニメデ",
+//       feedback: "正解！ガニメデは、木星の第三惑星だよ！",
+//     },
+//   ],
+// }
+
+// quiz を画面に表示する関数
+// const reloadQuiz = function () {
+//   // 問題文を表示
+//   quizText.textContent = "Q. " + quiz.text
+
+//   // 画像を表示
+//   quizImage.src = "./images/" + quiz.image
+
+//   // 選択肢（ボタン）の中身を表示
+//   choice1.textContent = quiz.choices[0].text
+//   choice2.textContent = quiz.choices[1].text
+//   choice3.textContent = quiz.choices[2].text
+// }
+
+// // choiceNumber番目の選択肢を選択
+// const choose = function (choiceNumber) {
+//   // フィードバックを表示
+//   feedback.textContent = quiz.choices[choiceNumber].feedback
+// }
+
+// choice1.onclick = function () {
+//   // 0 番目の選択肢を選択
+//   choose(0)
+// }
+// choice2.onclick = function () {
+//   // 1 番目の選択肢を選択
+//   choose(1)
+// }
+// choice3.onclick = function () {
+//   // 2 番目の選択肢を選択
+//   choose(2)
+// }
+
+// // reloadQuiz関数 を実行して、クイズを画面に表示する
+// reloadQuiz()
+
+//-------------------------------------------------------//
+//マイクイズ
 const quiz = {
-  text: "この星の名前は何でしょう？",
-  image: "Ganymede.jpg",
+  text: "私の研究内容は何でしょう？",
+  // image: "Ganymede.jpg",
   choices: [
     {
-      text: "ゴリアテ",
-      feedback:
-        "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。",
+      text: "宇宙物理学",
+      feedback: "正解！ブラックホールの研究をしています！",
     },
     {
-      text: "ゼニガメ",
-      feedback: "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。",
+      text: "素粒子物理学",
+      feedback: "残念！素粒子物理は学部時代に心が折れてやめました。",
     },
     {
-      text: "ガニメデ",
-      feedback: "正解！ガニメデは、木星の第三惑星だよ！",
+      text: "プラズマ物理学",
+      feedback: "残念！プラズマ物理は授業内容がつまらなすぎてやめました。",
     },
   ],
 }
@@ -32,7 +89,7 @@ const reloadQuiz = function () {
   quizText.textContent = "Q. " + quiz.text
 
   // 画像を表示
-  quizImage.src = "./images/" + quiz.image
+  // quizImage.src = "./images/" + quiz.image
 
   // 選択肢（ボタン）の中身を表示
   choice1.textContent = quiz.choices[0].text
