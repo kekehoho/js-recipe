02ce6e5537d8a1fef2d45815403c379bead56c73
kekehoho <tkbys.univ01061@outlook.jp>
2022-10-14 13:27:32 +0900

    Cat or Cocktail

--- 03_サンプル/02_DogAPIで非同期処理/main.js
+++ 03_サンプル/02_DogAPIで非同期処理/main.js
@@ -1,19 +1,39 @@
-const imageElement = document.getElementById("dog-image")
+const imageElement = document.getElementById("cocktail-image")
 
-// 指定したサーバーにデータを取りに行く
+//指定したサーバーにデータを取りに行く
 // fetch("https://dog.ceo/api/breeds/image/random")
 //   .then((res) => {
 //     return res.json() // 結果を json として読み込む
 //   })
 //   .then((data) => {
 //     imageElement.src = data.message // 画像を表示する
+//     console.log(data)
 //   })
 
 //rennshuu
-fetch("https://dog.ceo/api/breeds/image/random")
+// fetch("https://dog.ceo/api/breeds/image/random")
+//   .then((res) => {
+//     return res.json()
+//   })
+//   .then((data) => {
+//     imageElement.src = data.message
+//   })
+
+//カクテル
+fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
   .then((res) => {
     return res.json()
   })
   .then((data) => {
-    imageElement.src = data.message
+    imageElement.src = data.drinks[0].strDrinkThumb
   })
+
+//ネコ
+// fetch("https://api.thecatapi.com/v1/images/search")
+//   .then((res) => {
+//     return res.json()
+//   })
+//   .then((json) => {
+//     imageElement.src = json[0].url
+//     console.log(json[0].url)
+//   })
