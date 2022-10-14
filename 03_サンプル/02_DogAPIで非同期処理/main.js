0e992424ac9e6f21804a562590aeb7333074fc11
kekehoho <tkbys.univ01061@outlook.jp>
2022-10-14 12:57:27 +0900

    [DogAPIで非同期処理]を学習しました。

--- 03_サンプル/02_DogAPIで非同期処理/main.js
+++ 03_サンプル/02_DogAPIで非同期処理/main.js
@@ -1,10 +1,19 @@
 const imageElement = document.getElementById("dog-image")
 
 // 指定したサーバーにデータを取りに行く
+// fetch("https://dog.ceo/api/breeds/image/random")
+//   .then((res) => {
+//     return res.json() // 結果を json として読み込む
+//   })
+//   .then((data) => {
+//     imageElement.src = data.message // 画像を表示する
+//   })
+
+//rennshuu
 fetch("https://dog.ceo/api/breeds/image/random")
   .then((res) => {
-    return res.json() // 結果を json として読み込む
+    return res.json()
   })
   .then((data) => {
-    imageElement.src = data.message // 画像を表示する
+    imageElement.src = data.message
   })
