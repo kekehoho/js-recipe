96285909c03c4806d98d32f3f7c694e4b48464bb
kekehoho <tkbys.univ01061@outlook.jp>
2022-10-15 20:04:51 +0900

    自動販売機

--- 03_サンプル/02_DogAPIで非同期処理/main.js
+++ 03_サンプル/02_DogAPIで非同期処理/main.js
@@ -1,15 +1,60 @@
-const imageElement = document.getElementById("cocktail-image")
-const button = document.getElementById("button")
+const imageElement = document.getElementById("dog-image")
+const select = document.getElementById("select")
+const button = document.getElementById("confirm")
+const options = select.options
+
+console.log(select.options[0].value)
+// const button = document.getElementById("button")
 
 //指定したサーバーにデータを取りに行く
-// fetch("https://dog.ceo/api/breeds/image/random")
-//   .then((res) => {
-//     return res.json() // 結果を json として読み込む
-//   })
-//   .then((data) => {
-//     imageElement.src = data.message // 画像を表示する
-//     console.log(data)
-//   })
+button.onclick = function () {
+  if (options[0].selected) {
+    fetch("https://dog.ceo/api/breeds/list/all")
+      .then((res) => {
+        return res.json() // 結果を json として読み込む
+      })
+      .then((data) => {
+        imageElement.src = data.message.african[0] // 画像を表示する
+        console.log(data.message.african)
+      })
+  } else if (options[1].selected) {
+    fetch("https://dog.ceo/api/breeds/list/all")
+      .then((res) => {
+        return res.json() // 結果を json として読み込む
+      })
+      .then((data) => {
+        imageElement.src = data.message // 画像を表示する
+        console.log(data)
+      })
+  } else if (options[2].selected) {
+    fetch("https://dog.ceo/api/breeds/list/all")
+      .then((res) => {
+        return res.json() // 結果を json として読み込む
+      })
+      .then((data) => {
+        imageElement.src = data.message // 画像を表示する
+        console.log(data)
+      })
+  } else if (options[3].selected) {
+    fetch("https://dog.ceo/api/breeds/list/all")
+      .then((res) => {
+        return res.json() // 結果を json として読み込む
+      })
+      .then((data) => {
+        imageElement.src = data.message // 画像を表示する
+        console.log(data)
+      })
+  } else {
+    fetch("https://dog.ceo/api/breeds/list/all")
+      .then((res) => {
+        return res.json() // 結果を json として読み込む
+      })
+      .then((data) => {
+        imageElement.src = data.message // 画像を表示する
+        console.log(data)
+      })
+  }
+}
 
 //rennshuu
 // fetch("https://dog.ceo/api/breeds/image/random")
@@ -21,15 +66,15 @@ const button = document.getElementById("button")
 //   })
 
 //カクテル
-button.onclick = function () {
-  fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-    .then((res) => {
-      return res.json()
-    })
-    .then((data) => {
-      imageElement.src = data.drinks[0].strDrinkThumb
-    })
-}
+// button.onclick = function () {
+//   fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+//     .then((res) => {
+//       return res.json()
+//     })
+//     .then((data) => {
+//       imageElement.src = data.drinks[0].strDrinkThumb
+//     })
+// }
 
 //ネコ
 // fetch("https://api.thecatapi.com/v1/images/search")
